c24b1010b0eca9977073f0769df94695144ad09f
Sean Murphy <sean@iamseanmurphy.com>
2026-02-24 09:31:56 +0000

    8.0.1

--- jetpack-social/jetpack_vendor/automattic/jetpack-publicize/build/classic-editor.asset.php
+++ jetpack-social/jetpack_vendor/automattic/jetpack-publicize/build/classic-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '5991a370196f168d041a');
+<?php return array('dependencies' => array('jquery', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '6e4675dfa20ab768c4b6');
